1f9aebe92914063431c4b4de9ef50316159c219d
yohantek <yohannesteklay7@gmail.com>
2024-07-02 23:13:42 +0200

    built in function, and how to use them is explored

--- buildinfunctions.py
+++ buildinfunctions.py
@@ -1,3 +1,9 @@
 devices= ["ASA", "NEXUS", "CATALYST", "NEXUS", "asa"]
 
 
+dir(devices) # this will show us the available functions for the object devices
+
+#count , is one of the functions, and to see how we can use count
+
+help(devices.count)
+
