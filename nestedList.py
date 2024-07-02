0fa0936e29a8895f1100e6b4589c118337fc6bfc
yohantek <yohannesteklay7@gmail.com>
2024-07-02 23:10:48 +0200

    added another python file about nestedLists

---
+++ nestedList.py
@@ -0,0 +1,29 @@
+
+
+nested_list = [
+    1,
+    [2, 3, [4, 5]],
+    [6, [7, 8], 9],
+    10,
+    [[11, 12], 13, [14, [15, 16]]],
+    [17, [18, [19, 20]]]
+]
+
+def find_positions(nested_list, value, path=None):
+    if path is None:
+        path = []
+    
+    positions = []
+    
+    for index, item in enumerate(nested_list):
+        current_path = path + [index]
+        if isinstance(item, list):
+            positions.extend(find_positions(item, value, current_path))
+        elif item == value:
+            positions.append(current_path)
+    
+    return positions
+
+values_to_find = [4, 13, 20]
+positions = {value: find_positions(nested_list, value) for value in values_to_find}
+positions
